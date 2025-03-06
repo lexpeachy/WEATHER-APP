919481340814af8438639b3b0e97a1ffe821539b
evans kirimi <evanskirimi483@gmail.com>
2025-03-06 16:27:03 +0300

    creating a weather app

---
+++ weather/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+import json
+import urllib.request
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+    else:
+        city = ''
+    return render(request, 'index.html')
